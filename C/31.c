1b19768c4758fec3e20b66c7318b2ece21974abe
Shuaib Abubakker Bapputty Haji <42908782+ShuaibABH@users.noreply.github.com>
2019-05-08 10:01:55 +0530

    Update 31.c

--- C/31.c
+++ C/31.c
@@ -1,18 +1,29 @@
 #include<stdio.h>
 void main(){
-	int arr[10], i=0, n, j=0, k=5;
+	int arr[10], i=0, n, j=0,k;
 	arr[0] = 1;
+	printf("Enter the no.of elements: ");
+        scanf("%d", &k);
 	printf("Enter the array");
-	for(i=0; i < 5; i++)scanf("%d", &arr[i]);
+	for(i=0; i < k; i++)
+          scanf("%d", &arr[i]);
+
 	printf("Enter element to remove:");
 	scanf("%d", &n);
-	for(i=0; i<k; i++){
-		if(arr[i] == n){
-			for(j=i; j<k; j++){
+
+	for(i=0; i<k; i++)
+        {
+		if(arr[i] == n)
+                {
+			for(j=i; j<k-1; j++)
+                        {
 				arr[j] = arr[j+1];
 			}
 			k--;
+                        i--;
 		}
-		printf("%d\n", arr[i]);
+
 	}
+        for(i=0; i < k; i++)
+        printf("%d\n", arr[i]);
 }
