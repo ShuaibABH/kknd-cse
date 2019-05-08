e112d487368dc33a6ba171321dbc54c887bacde6
Shuaib Abubakker Bapputty Haji <42908782+ShuaibABH@users.noreply.github.com>
2019-05-08 08:06:09 +0530

    Update 19.c

--- C/19.c
+++ C/19.c
@@ -1,20 +1,24 @@
 #include<stdio.h>
 #include<math.h>
 int fact(int a)
-{int m,i;
+{int m=1,i;
 for(i=1;i<=a;i++)
-	m=m*1;
+	m=m*i;
 return m;
 }
 
 void main()
 {int n,x,i,a=0;
-printf("Enter the limiting no of taylor series ");
+printf("Enter the limiting no of taylor series :");
 scanf("%d",&n);
-printf("Enter the value of x");
+printf("Enter the value of x: ");
 scanf("%d",&x);
 
 for(i=1;i<=n;i++)
-	a=a+((pow(x,(2*i-1))*pow(-1,i+1))/fact(2*i-1))
-printf("the aprrox of sinx is %d",a);
+	a=a+((pow(x,(2*i-1))*pow(-1,i+1))/fact(2*i-1));
+printf("the aprrox of sinx is %d ",a);
 }
+
+
+//we want run the terminal as below because to include math library, lm means libmath
+//gcc (filename).c -lm 
